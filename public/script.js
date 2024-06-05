60a09ad8b12854c12c2829917e35fefa9423c3f0
xenuver02 <gabidxenuver@gmail.com>
2024-06-05 10:41:12 +0700

    addanimation

--- public/script.js
+++ public/script.js
@@ -1,37 +1,35 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+    let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    };
 
+    let sections = document.querySelectorAll('section');
+    let navlinks = document.querySelectorAll('header nav a');
 
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetLeft - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+            if (top >= offset && top < offset + height) {
+                navlinks.forEach(link => {
+                    link.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+            }
+        });
 
-        if(top >= offset && top > offset + height) {
-            navlinks.forEach(link => {
-                links.classlist.remove('active');
-                document.querySelector('header nav a[herf*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-    let header = document.querySelector('header');
+        let header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 100);
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    };
 
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
-
-
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact-form', { origin: 'botto' });
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact-form', { origin: 'bottom' });
